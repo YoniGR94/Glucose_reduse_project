62d86fe43cd55ea7155b61c1faa170271a597a5c
YonigR <yonig94@gmail.com>
2022-11-13 11:23:43 +0200

    1st edition

---
+++ sweet table scaling to normal.R
@@ -0,0 +1,28 @@
+###
+#Import & scaling glucose data
+###
+
+library(tidyverse)
+library(scales)
+library(openxlsx)
+
+sweets<- read_excel(
+  #"//netapp/users/Shyonatang/divuach_for_R/Project_Sweet.xlsx",
+  'C:/Users/yonig/Desktop/data for regretion/Project_Sweet.xlsx',
+  sheet = 'gluc_data', col_names = T) %>%
+  mutate(Glucose_result= scale(Glucose_result)
+  )
+write.xlsx(sweets,file = "data/sweets.xls")
+
+day_score<- read_excel(
+  "C:/Users/yonig/Desktop/data for regretion/Project_Sweet.xlsx",
+  sheet = 'day_list', col_names = T)
+
+write.xlsx(day_score,file = "data/day_score.xls")
+
+
+index_key<- read_excel(
+  "C:/Users/yonig/Desktop/data for regretion/Project_Sweet.xlsx",
+  sheet = 'index key', col_names = T)
+
+write.xlsx(index_key,file = "data/index key.xls")
